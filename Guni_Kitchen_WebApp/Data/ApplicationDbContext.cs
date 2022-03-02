b35b05d6d3def55b52b48c2030a9eca68952ac9c
Mayur-Mistry <mayurmistry022@gmail.com>
2022-03-02 15:02:40 +0530

    Added The Customized Identity User && Role

--- Guni_Kitchen_WebApp/Data/ApplicationDbContext.cs
+++ Guni_Kitchen_WebApp/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Guni_Kitchen_WebApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -6,7 +7,8 @@ using System.Text;
 
 namespace Guni_Kitchen_WebApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext 
+                        : IdentityDbContext<MyIdentityUser,MyIdentityRole,Guid>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
