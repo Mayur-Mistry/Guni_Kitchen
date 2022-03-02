e3487805f5dcc1d0be578f3701e63ac378517e9c
Mayur-Mistry <mayurmistry022@gmail.com>
2022-03-02 13:01:10 +0530

    Added Identity

---
+++ Guni_Kitchen_WebApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Guni_Kitchen_WebApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Guni_Kitchen_WebApp.Areas.Identity.IdentityHostingStartup))]
+namespace Guni_Kitchen_WebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
